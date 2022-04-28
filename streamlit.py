0a7fdaf90b3d38d726bf305a4be75eb0972e9291
ongjoel <102646193+ongjoel@users.noreply.github.com>
2022-04-29 07:27:41 +0800

    logging

--- streamlit.py
+++ streamlit.py
@@ -37,7 +37,7 @@ from pyspark.sql import functions as f
 from pyspark.sql import SparkSession
 
 sc = SparkSession.builder.appName("word2vec").config("spark.driver.memory", "10g").getOrCreate()
-logging.getLogger("py4j").setLevel(logging.ERROR)
+sc.logging.getLogger("py4j").setLevel(logging.ERROR)
 
 #@st.cache(allow_output_mutation=True, suppress_st_warning=True, hash_funcs={"MyUnhashableClass": lambda _: None})
 #def load_indexed():
